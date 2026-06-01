c0319089847316f9d29c3c75b1bd694a568d79fa
Manoj Khanal <manojkhanal936@gmail.com>
2026-05-31 19:13:17 -0500

    Initial Project Setup

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class User(AbstractUser):
+
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        MANAGER = "MANAGER", "Manager"
+        DEVELOPER = "DEVELOPER", "Developer"
+
+    email = models.EmailField(unique=True)
+
+    role = models.CharField(
+        max_length=20,
+        choices = Role.choices,
+        default=Role.DEVELOPER
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
